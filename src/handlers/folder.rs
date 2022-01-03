2c276d6754438fdc0d1a53958d276486ac736216
David Dvořák <dejviddvorak146@gmail.com>
2022-01-03 19:10:55 +0100

    debug folder get api path

--- src/handlers/folder.rs
+++ src/handlers/folder.rs
@@ -1,50 +1,60 @@
-use actix_web::{HttpResponse, HttpRequest, Error};
-use std::env;
-use std::time::SystemTime;
-use std::fs;
 use crate::utils::valid_path;
+use actix_web::{Error, HttpRequest, HttpResponse};
+use std::env;
+use std::fs;
+use std::time::SystemTime;
 
-use crate::models::File;
 use crate::middleware::{CanDownload, CanUpload};
+use crate::models::File;
 use crate::reserr::ResErr;
 
 pub async fn get_folder(token: CanDownload, req: HttpRequest) -> Result<HttpResponse, ResErr> {
-
-    let path: String = (env::var("CLOUD_PATH").unwrap()+&token.path+"/"+req.match_info().query("filename")).parse()
-        .map_err(|_| ResErr::BadClientData("cant parse path"))?;
+    let path: String =
+        (env::var("CLOUD_PATH").unwrap() + &token.path + "/" + req.match_info().query("filename"))
+            .parse()
+            .map_err(|_| ResErr::BadClientData("cant parse path"))?;
 
     valid_path(&path).map_err(|err| ResErr::BadClientData(err))?;
 
-    let paths = fs::read_dir(path)
-        .map_err(|_| ResErr::BadClientData("cant find path"))?;
+    let paths = fs::read_dir(path).map_err(|_| ResErr::BadClientData("cant find path"))?;
 
     let mut res: Vec<File> = Vec::new();
     let now = SystemTime::now();
 
     for path in paths {
-        let better_path = path.map_err(|_| ResErr::BadClientData("cant do path"))?.path();
-        let metadata = fs::metadata(&better_path).map_err(|_| ResErr::BadClientData("cant get metadata"))?;
+        let better_path = path
+            .map_err(|_| ResErr::BadClientData("cant do path"))?
+            .path();
+        let metadata =
+            fs::metadata(&better_path).map_err(|_| ResErr::BadClientData("cant get metadata"))?;
         res.push(File {
-            name: better_path.clone().into_os_string().into_string().unwrap().split('/').last().unwrap().to_string(),
+            name: better_path
+                .to_str()
+                .unwrap()
+                .replace("\\", "/")
+                .split('/')
+                .last()
+                .unwrap()
+                .to_string(),
             date: match metadata.modified() {
                 Ok(v) => match now.duration_since(v) {
                     Ok(v) => v.as_secs().to_string(),
-                    Err(_) => "time is broken".to_string()
+                    Err(_) => "time is broken".to_string(),
                 },
-                Err(_) => "no supported".to_string()
+                Err(_) => "no supported".to_string(),
             },
-            size: metadata.len()
+            size: metadata.len(),
         });
     }
 
     Ok(HttpResponse::Ok().json(res))
-
 }
 
 pub async fn create_folder(token: CanUpload, req: HttpRequest) -> Result<HttpResponse, ResErr> {
-    
-    let path: String = (env::var("CLOUD_PATH").unwrap()+&token.path+"/"+req.match_info().query("filename")).parse()
-        .map_err(|_| ResErr::BadClientData("cant parse path"))?;
+    let path: String =
+        (env::var("CLOUD_PATH").unwrap() + &token.path + "/" + req.match_info().query("filename"))
+            .parse()
+            .map_err(|_| ResErr::BadClientData("cant parse path"))?;
 
     valid_path(&path).map_err(|err| ResErr::BadClientData(err))?;
 
@@ -54,8 +64,9 @@ pub async fn create_folder(token: CanUpload, req: HttpRequest) -> Result<HttpRes
 }
 
 pub async fn delete_folder(token: CanUpload, req: HttpRequest) -> Result<HttpResponse, Error> {
-    
-    let path: String = (env::var("CLOUD_PATH").unwrap()+&token.path+"/"+req.match_info().query("filename")).parse()?;
+    let path: String =
+        (env::var("CLOUD_PATH").unwrap() + &token.path + "/" + req.match_info().query("filename"))
+            .parse()?;
 
     valid_path(&path).map_err(|err| ResErr::BadClientData(err))?;
 
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 // dependencies
-use actix_web::{ web, App, HttpServer, error, HttpResponse};
+use actix_files as fs;
+use actix_web::{error, web, App, HttpResponse, HttpServer};
 use r2d2_sqlite::{self, SqliteConnectionManager};
 use std::env;
-use actix_files as fs;
 
 // modules
 mod handlers;
-mod models;
 mod jwt;
 mod middleware;
-mod utils;
+mod models;
 mod reserr;
+mod utils;
 
 mod db;
 use db::{create_tables, Pool};
-
+use reserr::ResErr;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
-    
+
     let manager = SqliteConnectionManager::file("data.db");
     let pool = Pool::new(manager).unwrap();
 
@@ -40,8 +40,14 @@ async fn main() -> std::io::Result<()> {
             .data(pool.clone())
             // admin utils
             .route("/users", web::get().to(handlers::admin::get_users))
-            .route("/users/{id}", web::get().to(handlers::admin::get_user_by_id))
-            .route("/users/{id}", web::delete().to(handlers::admin::delete_user))
+            .route(
+                "/users/{id}",
+                web::get().to(handlers::admin::get_user_by_id),
+            )
+            .route(
+                "/users/{id}",
+                web::delete().to(handlers::admin::delete_user),
+            )
             .route("/users/{id}", web::patch().to(handlers::admin::update_user))
             .route("/users", web::post().to(handlers::admin::add_user))
             // user utils
@@ -51,23 +57,39 @@ async fn main() -> std::io::Result<()> {
             .route("/login", web::post().to(handlers::login::login))
             .route("/check_login", web::post().to(handlers::login::check_login))
             // cloud utils
-            .route("/file/{filename:.*}", web::get().to(handlers::file::get_file))
-            .route("/file/{filename:.*}", web::post().to(handlers::file::post_file))
-            .route("/file/{filename:.*}", web::delete().to(handlers::file::delete_file))
-            // folder utils
-            .route("/folder/{filename:.*}", web::get().to(handlers::folder::get_folder))
-            .route("/folder/{filename:.*}", web::post().to(handlers::folder::create_folder))
-            .route("/folder/{filename:.*}", web::delete().to(handlers::folder::delete_folder))
-            .service(fs::Files::new("/", "./static"))
-            .default_service(
-                web::route().to(err404)
+            .route(
+                "/file/{filename:.*}",
+                web::get().to(handlers::file::get_file),
             )
+            .route(
+                "/file/{filename:.*}",
+                web::post().to(handlers::file::post_file),
+            )
+            .route(
+                "/file/{filename:.*}",
+                web::delete().to(handlers::file::delete_file),
+            )
+            // folder utils
+            .route(
+                "/folder/{filename:.*}",
+                web::get().to(handlers::folder::get_folder),
+            )
+            .route(
+                "/folder/{filename:.*}",
+                web::post().to(handlers::folder::create_folder),
+            )
+            .route(
+                "/folder/{filename:.*}",
+                web::delete().to(handlers::folder::delete_folder),
+            )
+            .service(fs::Files::new("/", "./static"))
+            .default_service(web::route().to(err404))
     })
     .bind(env::var("ADDRESS").unwrap())?
     .run()
     .await
 }
 
-async fn err404() -> String {
-    return String::from("error 404");
+async fn err404() -> Result<HttpResponse, ResErr> {
+    Err(ResErr::BadClientData("error 404"))
 }
